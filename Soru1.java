1e1f4fc5c637e27720c286ff0984d07148331697
ainagulyarba@gmail.com <ainagulyarba@gmail.com>
2021-12-25 21:33:12 +0100

    aa

---
+++ Soru1.java
@@ -0,0 +1,24 @@
+package practiceSorular;
+
+import java.util.Arrays;
+
+public class Soru1 {
+
+	public static void main(String[] args) {
+		/*1---Sayının Sayı değerlerinin toplamasını yapan Java kodu yazınız. ​ Test
+		 * Data 34 Beklenen Çıktı 7
+		 */
+		
+		String sayi="67";
+		
+		String sayi1[]=sayi.split("");
+		System.out.println(Arrays.toString(sayi1));
+		int toplam=0;
+		for (String w : sayi1) {
+			toplam+=Integer.parseInt(w);
+		}
+		
+		System.out.println(toplam);
+	}
+
+}
