2470e9d160dddb0fa8f0a80ed09eaedc32318ba5
ainagulyarba <ainagulyarba@gmail.com>
2022-01-04 20:29:41 +0100

    vV

---
+++ deger.java
@@ -0,0 +1,23 @@
+package ainagulOdev1;
+
+public class deger {
+
+	public static void main(String[] args) {
+
+		// Primitive data type boolean, char, byte, short, integer,long, double, float
+		// byte, short, integer,long, double, float veri tiplerinin max ve min
+		// degerlerini yazdiriniz
+		
+		Short sayi = 10;
+		System.out.println(sayi.MAX_VALUE);
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
